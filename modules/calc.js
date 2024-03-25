107ff3fb8642f2437df89b0c9a3f387eb69f6f60
amarmern <amarmca51@gmail.com>
2024-03-25 12:03:32 +0530

    chanes the module

--- modules/calc.js
+++ modules/calc.js
@@ -1,17 +1,29 @@
 // File name: calc.js
 
-exports.add = function (x, y) {
-  return x + y;
-};
+// exports.add = function (x, y) {
+//   return x + y;
+// };
 
-exports.sub = function (x, y) {
-  return x - y;
-};
+// exports.sub = function (x, y) {
+//   return x - y;
+// };
 
-exports.mult = function (x, y) {
-  return x * y;
-};
+// exports.mult = function (x, y) {
+//   return x * y;
+// };
 
-exports.div = function (x, y) {
-  return x / y;
+// exports.div = function (x, y) {
+//   return x / y;
+// };
+
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+const mul = (x, y) => x * y;
+const div = (x, y) => x / y;
+
+module.exports = {
+  add,
+  sub,
+  mul,
+  div,
 };
