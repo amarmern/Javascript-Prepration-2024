f3451f345797c4be80442a636a54d7e71a9a4262
amarmern <amarmca51@gmail.com>
2026-04-09 10:48:00 +0530

    added code

--- test1.js
+++ test1.js
@@ -1,1 +1,15 @@
-console.log(isValid('{[[]}'));
+const { tokenToString } = require('typescript');
+
+app.get((req, res) => {
+  const page = req.params.page || 1;
+  const limit = req.params.skip || 10;
+
+  const skip = (page - 1) * limit;
+
+  const users = User.find().skip(skip).limit(limit);
+  const totalUsers = users.length;
+  res.status(200).send.json({
+    totalPage: Math.ceil(totalUsers / limit),
+    data: users,
+  });
+});
